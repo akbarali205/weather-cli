6cfba49c3f1b63fc82472811da7aad6211852827
akbarali404 <akbarsobirov226@gmail.com>
2024-12-04 10:15:13 +0500

    save token

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import getArgs from './helpers/argv.js'
 import {printError, printHelp, printSuccess} from './services/log.service.js'
+import {getKeyValue, saveKeyValue} from './services/storage.service.js'
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Token saved successfully')
+    } catch (error) {
+        printError('Error saving token', error)
+    }
+}
 
 const startCLI = () => {
     const args = getArgs(process.argv);
@@ -13,6 +23,7 @@ const startCLI = () => {
     }
     if (args.t) {
         // save token
+        return saveToken(args.t)
     }
     // result
 }
